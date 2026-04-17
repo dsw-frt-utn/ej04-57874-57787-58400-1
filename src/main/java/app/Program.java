8a0ee36f3baca4a83db92829967fd578617a7d1d
joel <angeljoeltoledo@gmail.com>
2026-04-17 15:55:40 -0300

    Agregacion del MenuPrincipal

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -7,9 +7,10 @@ import views.*;
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
-        IngresarVehiculos view2 = new IngresarVehiculos();
-        view2.setVisible(true);
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipal().setVisible(true);
+            }
+        });
     }
 }
